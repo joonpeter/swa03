09b2b34a851812f3f3a94b4769e8f5422b686780
joonpeter <68453765+joonpeter@users.noreply.github.com>
2026-01-01 16:05:52 +0900

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-	baseURL: 'http://asp01-f4hvd6dccpbpawhr.koreacentral-01.azurewebsites.net',
+	baseURL: 'https://asp01-container-engthcgagzezhgbb.koreacentral-01.azurewebsites.net',
 })
 
 instance.defaults.headers.post['Content-Type'] =
